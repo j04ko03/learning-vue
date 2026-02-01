3873d6f31f8fe6fb9f572276b5a220c8f4d1fe42
Joako <joaquinmscollo@gmail.com>
2026-02-01 17:22:03 +0100

    Segunda lección VUE

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+import imagenCalcetinVerde from './assets/images/socks_green.jpeg'
+import imagenCalcetinAzul from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
+const imagen = ref(imagenCalcetinVerde)
+const url = ref ('https://www.vuemastery.com/courses/intro-to-vue-3-comp-api/introduction-comp-api')
+
 
 </script>
 
@@ -9,6 +14,8 @@ const product = ref('Socks')
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
+        <img v-bind:src="imagen" alt="Calcetinez verdes VUE.">
+        <a :href="url">Ver más info</a>
         <!-- image goes here -->
       </div>
       <div class="product-info">
