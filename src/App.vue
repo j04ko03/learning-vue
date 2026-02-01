21070fc5472efb7e2efefd6f8ab999013a339074
Joako <joaquinmscollo@gmail.com>
2026-02-01 17:36:50 +0100

    Tercera lección VUE

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
+// const disp = ref(true)
+const inv = ref(5)
+const enVenta = ref(true)
 
 </script>
   
@@ -16,6 +19,10 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="enVenta">Disponible</p>
+        <!-- <p v-show="disp">Disponible</p> -->
+        <p v-else-if="inv <= 10 && inv > 0">Casi sin existencia</p>
+        <p v-else>No disponible</p>
       </div>
     </div>
   </div>
