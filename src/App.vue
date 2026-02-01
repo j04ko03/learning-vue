749a5fed5339836742c53d0f073e820bd11c5141
Joako <joaquinmscollo@gmail.com>
2026-02-01 17:02:30 +0100

    Primera lección VUE

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-<script setup></script>
+<script setup>
+  import {ref} from 'vue';
+
+  const title = ref ('Calceines VUE');
+  const description = ref ('Par de calcetines VUE premium.')
+
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
       </div>
     </div>
   </div>
